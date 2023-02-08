b74069076c1fff016f79863d2a852ec7fdde8b24
Николаев Максим <nikolaev.mg@nizta.local>
2023-02-08 17:14:05 +0300

    added popup closing

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
 let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.getElementById('name');
+let professionInput = document.getElementById('profession');
+popup.classList.remove('popup_opened');
+
+function popupOpen() {
+  popup.classList.toggle('popup_opened');
+}
+
+function popupClosed() {
+  popup.classList.toggle('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+}
+
+editButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClosed);
