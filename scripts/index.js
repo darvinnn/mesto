fd485a7407c53bc65a9ba1b74d072293a4f38ba9
Николаев Максим <nikolaev.mg@nizta.local>
2023-02-09 14:17:13 +0300

    added js

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,23 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.getElementById('name');
-let professionInput = document.getElementById('profession');
+let nameInput = document.querySelector('#name');
+let professionInput = document.querySelector('#profession');
+let name = document.querySelector('.profile__name');
+let profession = document.querySelector('.profile__subtitle');
 popup.classList.remove('popup_opened');
 
-function popupOpen() {
-  popup.classList.toggle('popup_opened');
-}
-
-function popupClosed() {
+function popupActivate() {
   popup.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
+  name.textContent = nameInput.value;
+  profession.textContent = professionInput.value;
+  popupActivate()
 }
 
-editButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupClosed);
+formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', popupActivate);
+popupCloseButton.addEventListener('click', popupActivate);
