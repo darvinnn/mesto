40c317ecc79386702f0aa6c1751a4ea7d4e8b0db
Maksim Nikolaev <darwinnnn27@gmail.com>
2023-02-09 21:14:42 +0300

    3 iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let professionInput = document.querySelector('.popup__input_type_profession');
 let name = document.querySelector('.profile__name');
 let profession = document.querySelector('.profile__subtitle');
 
 function popupOpen() {
+  nameInput.value = name.textContent;
+  professionInput.value = profession.textContent;
   popup.classList.add('popup_opened');
 }
 
